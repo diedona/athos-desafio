9d6bb1b01e10a7bb14d60d0ebe4505c8d2e115de
Diego Doná <diedona@gmail.com>
2019-09-19 22:59:49 -0300

    test with db mock (IN MEMORY - EF CORE)

---
+++ DDonah.AthosDesafio.Test/CondiminioServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using DDonah.AthosDesafio.Domain;
+using DDonah.AthosDesafio.Infra.Generated;
+using DDonah.AthosDesafio.Services;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace DDonah.AthosDesafio.Test
+{
+    [TestFixture]
+    public class CondiminioServiceTest
+    {
+        private readonly AthosDesafioContext _db;
+
+        public CondiminioServiceTest()
+        {
+            _db = DbMock.GetContextWithData();
+        }
+        [Test]
+        public void Condominio_NaoDeveInserir_TipoResponsavelInvalido()
+        {
+            CondominioService condominioService = new CondominioService(_db);
+            Condominio condominioFail = getCondominioComResponsavelMorador();
+            Assert.Throws<ArgumentException>(() => condominioService.Save(condominioFail));
+        }
+
+        [Test]
+        public void Condominio_DeveInserir()
+        {
+            CondominioService condominioService = new CondominioService(DbMock.GetContextWithData());
+            Condominio condominioSuccess = getCondominioNormalizado();
+            Assert.DoesNotThrow(() => condominioService.Save(condominioSuccess));
+        }
+
+        private Condominio getCondominioComResponsavelMorador()
+        {
+            return new Condominio()
+            {
+                Nome = "TesteCondominio",
+                AdministradoraId = 1,
+                ResponsavelId = _db.Usuario.FirstOrDefault(x => x.Tipo.Equals("MORADOR")).Id
+            };
+        }
+
+        private Condominio getCondominioNormalizado()
+        {
+            return new Condominio()
+            {
+                Nome = "TesteCondominioNormal",
+                AdministradoraId = 1,
+                ResponsavelId = _db.Usuario.FirstOrDefault(x => x.Tipo.Equals("SINDICO")).Id
+            };
+        }
+    }
+}
---
+++ DDonah.AthosDesafio.Test/DbMock.cs
@@ -0,0 +1,46 @@
+﻿using DDonah.AthosDesafio.Domain;
+using DDonah.AthosDesafio.Infra.Generated;
+using Microsoft.EntityFrameworkCore;
+
+namespace DDonah.AthosDesafio.Test
+{
+    /// <summary>
+    /// https://gunnarpeipman.com/aspnet-core-ef-inmemory/
+    /// </summary>
+    public class DbMock
+    {
+        public static AthosDesafioContext GetContextWithData()
+        {
+            var options = new DbContextOptionsBuilder<AthosDesafioContext>()
+                              .UseInMemoryDatabase("athos-desafio-in-memory")
+                              .Options;
+
+            var context = new AthosDesafioContext(options);
+            context.Database.EnsureDeleted();
+
+            var adm1 = new Administradora() { Nome = "ADM 1" };
+            var adm2 = new Administradora() { Nome = "ADM 2" };
+            var adm3 = new Administradora() { Nome = "ADM 3" };
+
+            context.Administradora.AddRange(new Administradora[] { adm1, adm2, adm3 });
+
+            var cond1 = new Condominio() { Nome = "CONDO 1", Administradora = adm1 };
+            var cond2 = new Condominio() { Nome = "CONDO 2", Administradora = adm2 };
+            var cond3 = new Condominio() { Nome = "CONDO 3", Administradora = adm2 };
+
+            context.Condominio.AddRange(new Condominio[] { cond1, cond2, cond3 });
+
+            var usu1 = new Usuario() { Nome = "MORADOR", Email = "MORADOR@MORA.COM", Tipo = "MORADOR", CondominioNavigation = cond1 };
+            var usu2 = new Usuario() { Nome = "SINDICO", Email = "SINDICO@MORA.COM", Tipo = "SINDICO", CondominioNavigation = cond2 };
+            var usu3 = new Usuario() { Nome = "ADM", Email = "ADM@MORA.COM", Tipo = "ADM", CondominioNavigation = cond3 };
+
+            context.Usuario.AddRange(new Usuario[] { usu1, usu2, usu3 });
+
+            //
+
+            context.SaveChanges();
+
+            return context;
+        }
+    }
+}
