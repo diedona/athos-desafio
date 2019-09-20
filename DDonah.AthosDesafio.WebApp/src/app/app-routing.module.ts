b24924b0ea3fb9fdcece0dc8fa1697b3cf3fe76f
Diego Doná <diedona@gmail.com>
2019-09-20 10:10:28 -0300

    unnecessary import

--- DDonah.AthosDesafio.WebApp/src/app/app-routing.module.ts
+++ DDonah.AthosDesafio.WebApp/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
