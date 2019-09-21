0de51f8684e3c02341b660aad770b8950b6d4817
Diego Doná <diedona@gmail.com>
2019-09-21 17:56:55 -0300

    menus

--- DDonah.AthosDesafio.WebApp/src/app/layout/layout.module.ts
+++ DDonah.AthosDesafio.WebApp/src/app/layout/layout.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { MaterialModule } from '../shared/material.module';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MainNavComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule
+  ],
   exports: [MainNavComponent]
 })
 export class LayoutModule { }
